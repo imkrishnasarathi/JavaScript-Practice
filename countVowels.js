fcb61bc061d46cfcd6090281d31aa1f0b7059f26
imkrishnasarathi <85837080+imkrishnasarathi@users.noreply.github.com>
2023-07-21 13:35:00 +0000

    write a for loop to count vowels and add up to the count

--- countVowels.js
+++ countVowels.js
@@ -1,3 +1,8 @@
 const vowels = ["a", "e", "i", "o", "u"];
 let sentence = 'This is a very good sentence';
 let count = 0;
+for (let i of sentence.split('')){
+  if (vowels.includes(i)){
+    count+=1;
+  }
+}
